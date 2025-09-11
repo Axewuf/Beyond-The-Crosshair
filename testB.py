0d8aa6060a6a122c0ff1586adc91e94effe4190a
“Rodrigo <innovacioneducativa@udep.edu.pe>
2025-09-10 19:59:44 -0500

    creating list of shooters csv

--- testB.py
+++ testB.py
@@ -25,11 +25,25 @@ IGDB_api = APIinfo("https://api.igdb.com/v4/", "kyjymututmshrl6e6yj3tcqyokuogf")
 endpoint = 'games'#"release_dates"
 # Shooter genre = 5
 
-params = "limit: 10;"
+offset = 0
 
+max_items = 500
+counter = 0
+igdb_shooters = []
 
 requestURL = IGDB_api.baseURL + endpoint
-igdb_data = requests.post(requestURL, headers = IGDB_api.authHeader, data=params)
-igdb_data = igdb_data.json()
 
-print(igdb_data)
+while True:
+    params = f"fields *; where genres = (5); sort name asc; limit: {max_items}; offset {max_items*counter};"
+    print(f"Page {counter}")
+    igdb_data = requests.post(requestURL, headers = IGDB_api.authHeader, data=params)
+    igdb_data = igdb_data.json()
+    # print(f"igdb_data = {igdb_data}")
+    if igdb_data == []:
+        break 
+    igdb_shooters += igdb_data
+    counter += 1
+
+df_igdb_shooters = pd.DataFrame(igdb_shooters)
+df_igdb_shooters.to_csv("igdb_shooters.csv", index=False)
+# print(df_igdb_shooters)
