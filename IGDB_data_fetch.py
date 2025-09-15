1865ccafb88e0aaa03f52d8412dd9610a45a1e48
Axewuf <rodrigomorellib@hotmail.com>
2025-09-14 22:58:44 -0500

    Youtube API search

--- IGDB_data_fetch.py
+++ IGDB_data_fetch.py
@@ -15,10 +15,10 @@ class APIinfo:
         self.authHeader = {"Client-ID": "njp50qwu2nvpsx219fyqo3bzv63oin","Authorization": "Bearer "+token}
         self.baseURL = baseURL
 
-cs_data = pd.read_csv("assets/SteamDB Counter-Strike.csv")
-cs2_data = pd.read_csv("assets/SteamDB Counter-Strike 2.csv")
-css_data = pd.read_csv("assets/SteamDB Counter-Strike Source.csv")
-cscz_data = pd.read_csv("assets/SteamDB Counter-Strike Condition Zero.csv")
+# cs_data = pd.read_csv("assets/SteamDB Counter-Strike.csv")
+# cs2_data = pd.read_csv("assets/SteamDB Counter-Strike 2.csv")
+# css_data = pd.read_csv("assets/SteamDB Counter-Strike Source.csv")
+# cscz_data = pd.read_csv("assets/SteamDB Counter-Strike Condition Zero.csv")
 
 
 IGDB_api = APIinfo("https://api.igdb.com/v4/", "kyjymututmshrl6e6yj3tcqyokuogf")
